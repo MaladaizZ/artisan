9598c09c767087ff2d3d45ee29b47a5b87072b99
borys <b.marester@gmail.com>
2026-05-30 16:50:37 +0200

    ajout des dossiers pages et components

---
+++ frontend/src/components/header.jsx
@@ -0,0 +1,12 @@
+import Link from "react";
+import("./header.css")
+
+function Header (){
+    return(
+        <div>
+            <h1>Header en cours</h1>
+        </div>
+    )
+}
+
+export default Header
---
+++ frontend/src/pages/home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home () {
+    return (
+        <div>
+            <h1> Benvenur sur le site trouve ton Artisan</h1>
+        </div>
+    )
+}
+
+export default Home
