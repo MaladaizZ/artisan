3ad4f69eca60d1a006804296beee330ec9b638e6
borys <b.marester@gmail.com>
2026-06-08 15:34:57 +0200

    modification de service.js pour app.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,31 +1,28 @@
-import { useState } from 'react'
 import './App.css'
 import Footer from './components/footer'
 import Header from './components/header'
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route} from "react-router-dom";
 import Home from './pages/home'
 import About from './pages/about'
 import ArtisanListe from './pages/artisanliste'
 import ArtisanProfil from './pages/artisanprofil'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div>
     <Header/>
-      <Home />
 
-      <Footer />
-
-
-    </>
-  )
-}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/artisanliste" element={<ArtisanListe />} />
         <Route path="/artisanprofil" element={<ArtisanProfil />} />
       </Routes>
+    
+    <Footer />
+    </div>
+  )
+}
+
 export default App
