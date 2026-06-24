f3876c5816cc38ec805158e594b85eac4f48dcd7
borys <b.marester@gmail.com>
2026-06-24 17:06:02 +0200

    ajout du font graphik

--- frontend/src/pages/about.jsx
+++ frontend/src/pages/about.jsx
@@ -5,8 +5,10 @@ const About =() =>{
     return (
         <div>
 
-            <h1> Benvenue sur la page error 404</h1>
+            <h1> Benvenue sur la page Artisan</h1>
+            <h2>Vous trouverez bonheur parmis nos meilleurs artisans</h2>
         </div>
+        
     )
 }
 
